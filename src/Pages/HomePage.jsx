44c2bcbbb139be07443cfdbe43e7eb73856d5935
utsomondal <utsomondal2019@gmail.com>
2025-08-12 18:06:50 +0600

    react awesome reveal package used

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,14 +4,30 @@ import EuropeTravelTipsSection from "../Components/EuropeTravelTipsSection";
 import TestimonialSection from "../Components/TestimonialSection";
 import TouristsSpotSection from "../Components/TouristsSpotSection";
 
+import { Fade } from "react-awesome-reveal";
+
 const HomePage = () => {
   return (
     <>
-      <Banner />
-      <TouristsSpotSection />
-      <CountriesSection />
-      <EuropeTravelTipsSection />
-      <TestimonialSection />
+      <Fade triggerOnce>
+        <Banner />
+      </Fade>
+
+      <Fade triggerOnce delay={100}>
+        <TouristsSpotSection />
+      </Fade>
+
+      <Fade triggerOnce delay={200}>
+        <CountriesSection />
+      </Fade>
+
+      <Fade triggerOnce delay={300}>
+        <EuropeTravelTipsSection />
+      </Fade>
+
+      <Fade triggerOnce delay={400}>
+        <TestimonialSection />
+      </Fade>
     </>
   );
 };
