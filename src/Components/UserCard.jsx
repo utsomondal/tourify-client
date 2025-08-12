587aec12753f576bc839ebf451b03c85b8e2b54b
utsomondal <utsomondal2019@gmail.com>
2025-08-12 18:44:49 +0600

    firebase hosting added and fixes some minor ui issues

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useAuth } from "../Utils/useAuth";
 
 const UserCard = () => {
   const { user, logoutUser } = useAuth();
+  const [open, setOpen] = useState(false);
+
   const email = user?.email || "user@email.com";
   const initials = email.slice(0, 2).toUpperCase();
 
@@ -9,16 +12,24 @@ const UserCard = () => {
     logoutUser()
       .then(() => {
         console.log("User Logged Out");
+        setOpen(false);
       })
       .catch((error) => {
         console.log("error:", error);
       });
   };
 
+  const toggleDropdown = () => {
+    setOpen((toggle) => !toggle);
+  };
+
   return (
-    <div className="relative group">
-      {/* Avatar (hover trigger) */}
-      <div className="w-10 h-10 rounded-full flex items-center justify-center text-white text-base font-semibold cursor-pointer overflow-hidden bg-lm-primary hover:bg-lm-primary-hover dark:bg-dm-primary dark:hover:bg-dm-primary-hover">
+    <div className="relative">
+      {/* Avatar (click trigger) */}
+      <div
+        onClick={toggleDropdown}
+        className="w-10 h-10 rounded-full flex items-center justify-center text-white text-base font-semibold cursor-pointer overflow-hidden bg-lm-primary hover:bg-lm-primary-hover dark:bg-dm-primary dark:hover:bg-dm-primary-hover"
+      >
         {user?.photoURL ? (
           <img
             src={user.photoURL}
@@ -31,18 +42,20 @@ const UserCard = () => {
         )}
       </div>
 
-      {/* Dropdown on hover */}
-      <div className="absolute right-0 mt-2 w-64 bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 rounded-xl shadow-xl z-50 p-4 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300">
-        <p className="text-sm text-gray-800 dark:text-white mb-3 text-center">
-          {email}
-        </p>
-        <button
-          onClick={logout}
-          className="w-full px-4 py-2 text-sm rounded-lg bg-red-500 text-white hover:bg-red-600 transition cursor-pointer"
-        >
-          Logout
-        </button>
-      </div>
+      {/* Dropdown on click */}
+      {open && (
+        <div className="absolute right-0 mt-2 w-64 bg-white dark:bg-neutral-900 border border-gray-200 dark:border-neutral-700 rounded-xl shadow-xl z-50 p-4">
+          <p className="text-sm text-gray-800 dark:text-white mb-3 text-center">
+            {email}
+          </p>
+          <button
+            onClick={logout}
+            className="w-full px-4 py-2 text-sm rounded-lg bg-red-500 text-white hover:bg-red-600 transition cursor-pointer"
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 };
